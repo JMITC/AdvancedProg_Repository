4c78496f08bddc3fb0e30064d368c66a1e7f6edf
JMITC <flowdown94@gmail.com>
2015-10-21 10:51:12 +0100

    C++ Lab 1 question 4
    To determine if an integer input by the user is the same backwards as
    forwards.

---
+++ ProgLab2Q4/ProgLab2Q4/ProgLab2Q4.cpp
@@ -0,0 +1,13 @@
+// ProgLab2Q4.cpp : Defines the entry point for the console application.
+//Student Name: Jordan Martin
+//Student Number: c00166744
+//Program Function: To determine if an integer input by the user is the same backwards as forwards.
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
